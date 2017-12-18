8f97e2ac6e5e38ca1edbec23d201189f1e911c36
JonathanFrisby <34644120+JonathanFrisby@users.noreply.github.com>
2017-12-18 10:33:57 +0100

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -25,15 +25,44 @@ def HaarWavelet(array):
     for i in range(haararray.shape[0]):
         haararray[int(i/2),i]=1/2
         haararray[-int(i/2)-1,-i-1]=-1/2
+    for i in range(haararray.shape[0]//2):
+        haararray[-i-1,-2*i-1]=1/2
     b=dot(haararray,array)
     sm.imsave('AAA.jpg', b)
-    haararray=np.zeros((array.shape[1],array.shape[1]))
-    for i in range(haararray.shape[1]):
-        haararray[int(i/2),i]=1/2
-        haararray[-int(i/2)-1,-i-1]=-1/2
-    t=np.transpose(2*haararray)
+    haararray1=np.zeros((array.shape[1],array.shape[1]))
+    for i in range(haararray1.shape[1]):
+        haararray1[int(i/2),i]=1/2
+        haararray1[-int(i/2)-1,-i-1]=-1/2
+    for i in range(haararray1.shape[1]//2):
+        haararray1[-i-1,-2*i-1]=1/2
+    t=np.transpose(2*haararray1)
     e=dot(b,t)
     sm.imsave('AAB.jpg',e)
     
-HaarWavelet(Converttoarray('kvinna.jpg'))
+def Revert(array):
+    haararray=np.zeros((array.shape[0],array.shape[0]))
+    for i in range(haararray.shape[0]):
+        haararray[int(i/2),i]=1/2
+        haararray[-int(i/2)-1,-i-1]=-1/2
+    for i in range(haararray.shape[0]//2):
+        haararray[-i-1,-2*i-1]=1/2
+    haararray1=np.zeros((array.shape[1],array.shape[1]))
+    for i in range(haararray1.shape[1]):
+        haararray1[int(i/2),i]=1/2
+        haararray1[-int(i/2)-1,-i-1]=-1/2
+    for i in range(haararray1.shape[1]//2):
+        haararray1[-i-1,-2*i-1]=1/2
+    f=dot(np.transpose(haararray),dot(array,haararray1))
+    sm.imsave('AAC.jpg',f)
+    return print('hi')
+
+
+def HaarIterate(array,t=2):
+    for i in range(t+1):
+        HaarWavelet(array)
+        array=sm.imread('AAB.jpg',True)
+
+
+HaarIterate(Converttoarray('kvinna.jpg'))
+        
 
