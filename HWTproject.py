23656f17c198f877bcbbf82463af345a65388c51
JimKlintrup <34644107+JimKlintrup@users.noreply.github.com>
2017-12-18 11:30:14 +0100

    Update HWTproject.py

--- HWTproject.py
+++ HWTproject.py
@@ -21,13 +21,14 @@ if img.shape[0]%2!=0 or img.shape[1]%2!=0:   #Divide by 2 check remainder
 #Matrix construct
 #WM-matrix which will be multiplyed from the left
 WM=zeros((img.shape[0],img.shape[0]))   #Array with zeros shape number of rows
-for n in range(int(img.shape[0]/2)):    #Filling array with sqrt(2)/2
+for n in range(img.shape[0]//2):    #Filling array with sqrt(2)/2
 #First half of array
     WM[n,2*n]=sqrt(2)/2                 #Each row and row times 2
     WM[n,2*n+1]=sqrt(2)/2               #Each row and row times 2 + 1
 #Secong half of array
     WM[int(img.shape[0]/2)+n,2*n]=-sqrt(2)/2    #As previous only nalf way down
     WM[int(img.shape[0]/2)+n,2*n+1]=sqrt(2)/2
+
 #WNT-matrix, multiplyed from the right
 WNT=zeros((img.shape[1],img.shape[1]))  #Zero martix with shape of columns
 for n in range(int(img.shape[1]/2)):
@@ -37,6 +38,7 @@ for n in range(int(img.shape[1]/2)):
     WNT[int(img.shape[1]/2)+n,2*n]=-sqrt(2)/2
     WNT[int(img.shape[1]/2)+n,2*n+1]=sqrt(2)/2
 WNT=WNT.transpose()     #Matrix transpose in order for the effect to be latheral
+
 #Temp img comp
 imgdata=dot(WM,dot(img,WNT))
 
@@ -56,10 +58,28 @@ sm.imsave('da.jpg',da)
 dd=imgdata[int(imgdata.shape[0]/2):,int(imgdata.shape[1]/2):]   #Bottom right
 sm.imsave('dd.jpg',dd)
 
-#Invers Haar Transformation function
-inversWM=WM.transpose()
-inversWNT=WNT.transpose()
-inversdata=dot(inversWM,dot(imgdata,inversWNT))
-sm.imsave('inversimg.jpg',inversdata)
+#Reload of subarrays and stacking them.
 
+aa=sm.imread('aa.jpg',True)
+da=sm.imread('da.jpg',True)
+ad=sm.imread('ad.jpg',True)
+dd=sm.imread('dd.jpg',True)
+
+m=column_stack((aa,ad))
+n=column_stack((da,dd))
+mn=vstack((m,n))
+
+#Apply invers Haar Transformation.
+
+#Invers Haar Transformation function
+inversWM=WM.transpose()     #Transpose original Wavelet Matrices for revers transform
+inversWNT=WNT.transpose()
+inversdata=dot(inversWM,dot(imgdata,inversWNT)) #Appling invers to compress data
+sm.imsave('inversimg.jpg',inversdata)       #Save umcompressed data
+stackedmn=dot(inversWM,dot(mn,inversWNT))
+sm.imsave('wow2.jpg',stackedmn)
 #Haar Compression
+
+#Repeating compression and slicing n times
+
+#No Matrix multiplication
