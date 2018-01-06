7638e98ddead9758de25919fa159a587fd31b8fc
JonathanFrisby <34644120+JonathanFrisby@users.noreply.github.com>
2018-01-06 12:16:04 +0100

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -23,46 +23,43 @@ def Converttoarray(x):
 def HaarWavelet(array):
     haararray=np.zeros((array.shape[0],array.shape[0]))
     for i in range(haararray.shape[0]):
-        haararray[int(i/2),i]=(2**(1/2))/2
-        haararray[-int(i/2)-1,-i-1]=-(2**(1/2))/2
+        haararray[i//2,i]=sqrt(2)/2
+        haararray[-i//2-1,-i-1]=-sqrt(2)/2
     for i in range(haararray.shape[0]//2):
-        haararray[-i-1,-2*i-1]=(2**(1/2))/2
+        haararray[-i-1,-2*i-1]=sqrt(2)/2
     b=dot(haararray,array)
     haararray1=np.zeros((array.shape[1],array.shape[1]))
     for i in range(haararray1.shape[1]):
-        haararray1[int(i/2),i]=(2**(1/2))/2
-        haararray1[-int(i/2)-1,-i-1]=-(2**(1/2))/2
+        haararray1[i//2,i]=sqrt(2)/2
+        haararray1[-i//2-1,-i-1]=-sqrt(2)/2
     for i in range(haararray1.shape[1]//2):
-        haararray1[-i-1,-2*i-1]=(2**(1/2))/2
+        haararray1[-i-1,-2*i-1]=sqrt(2)/2
     t=np.transpose(haararray1)
     e=dot(b,t)
     sm.imsave('AAA.jpg',e)
-    aa=e[:int(e.shape[0]/2),:int(e.shape[1]/2)]
+    aa=e[:e.shape[0]//2,:e.shape[1]//2]
     sm.imsave('aa.jpg',aa)
-    ab=e[:int(e.shape[0]/2),int(e.shape[1]/2):]
+    ab=e[:e.shape[0]//2,e.shape[1]//2:]
     sm.imsave('ab.jpg',ab)
-    ac=e[int(e.shape[0]/2):,:int(e.shape[1]/2)] 
+    ac=e[e.shape[0]//2:,:e.shape[1]//2] 
     sm.imsave('ac.jpg',ac)
-    ad=e[int(e.shape[0]/2):,int(e.shape[1]/2):]  
+    ad=e[e.shape[0]//2:,e.shape[1]//2:]  
     sm.imsave('ad.jpg',ad)
-    m=column_stack((aa,ab))
-    n=column_stack((ac,ad))
-    mn=vstack((m,n))
-    return mn
+    return e
     
 def Revert(array):
     haararray=np.zeros((array.shape[0],array.shape[0]))
     for i in range(haararray.shape[0]):
-        haararray[int(i/2),i]=(2**(1/2))/2
-        haararray[-int(i/2)-1,-i-1]=-(2**(1/2))/2
+        haararray[i//2,i]=sqrt(2)/2
+        haararray[-i//2-1,-i-1]=-sqrt(2)/2
     for i in range(haararray.shape[0]//2):
-        haararray[-i-1,-2*i-1]=(2**(1/2))/2
+        haararray[-i-1,-2*i-1]=sqrt(2)/2
     haararray1=np.zeros((array.shape[1],array.shape[1]))
     for i in range(haararray1.shape[1]):
-        haararray1[int(i/2),i]=(2**(1/2))/2
-        haararray1[-int(i/2)-1,-i-1]=-(2**(1/2))/2
+        haararray1[i//2,i]=sqrt(2)/2
+        haararray1[-i//2-1,-i-1]=-sqrt(2)/2
     for i in range(haararray1.shape[1]//2):
-        haararray1[-i-1,-2*i-1]=(2**(1/2))/2
+        haararray1[-i-1,-2*i-1]=sqrt(2)/2
     f=dot(np.transpose(haararray),dot(array,haararray1))
     sm.imsave('AAC.jpg',f)
     return print('hi')
@@ -88,12 +85,8 @@ def Nomatrixcompress(array):
             Non[array.shape[0]//2+n,m]=(-(array[2*n,2*m])-array[2*n,2*m+1]+array[2*n+1,2*m]+array[2*n+1,2*m+1])/4
             Non[array.shape[0]//2+n,array.shape[1]//2+m]=(-array[2*n,2*m]+array[2*n,2*m+1]+array[2*n+1,2*m]-array[2*n+1,2*m+1])/4
     sm.imsave('AA#.jpg',Non)
-    Non=Non[:int(Non.shape[0]/2),:int(Non.shape[1]/2)]
+    Non=Non[:Non.shape[0]//2,:Non.shape[1]//2]
     sm.imsave('AA#2.jpg',Non)
 
-
-#Revert(HaarIterate(Converttoarray('kvinna.jpg'),3))
-#HaarIterate(Converttoarray('kvinna.jpg'),2)
-#Nomatrixcompress(Converttoarray('kvinna.jpg')))
 
 
